66ccb35a353b8bbe6b30445f40ba59cdfc9716f7
Lucia Liascovich <lucia.liascovich@gmail.com>
2022-08-30 21:23:58 -0300

    creación de proyecto

---
+++ Turnera/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Turnera.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
